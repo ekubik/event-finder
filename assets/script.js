9bdfd460f8fdcf083d549be571e67cc71d61b5b6
Ayako Woollan <ayako.woollan@gmail.com>
2022-01-06 21:56:33 +1100

    updating javascript

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// apikeys for google and Ticket Master
+const apiKeyG = "AIzaSyBiZYv6s5ocv7IYkiRQLleoe8MXRsMqtRc";
+const apiKeyTM = "8gCIaWZY4lHT8Aj0NRXlEobIThAGmNGO";
+
+// variables
+const currentTimeDiv = document.getElementById("current-date-time");
+const container = document.getElementById("container");
+const mapDiv = document.getElementById("map");
+const eventsListDiv = document.getElementById("events-list");
+
+// current time and day
+var today = moment();
+currentTimeDiv.text(today.format("dddd, Do MMM, YYYY"));
+currentTimeDiv.text(today.format("[at] h:mm a"));

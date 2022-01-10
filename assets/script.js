7725ac1b3d46d18e9d804146484f71244362f0c8
Ayako Woollan <ayako.woollan@gmail.com>
2022-01-10 20:11:29 +1100

    struggling

--- assets/script.js
+++ assets/script.js
@@ -27,3 +27,15 @@ fetch(geolocationApiUrl)
     userLongitude = data.longitude;
     userCity = data.city;
   });
+
+
+
+let map;
+initMap();
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}

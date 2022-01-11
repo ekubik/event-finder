b376045eddbbeaf6e173547a7ae647cbfd55393c
Nico Pasqualini <pasqualinin9@gmail.com>
2022-01-11 21:28:03 +1100

    refactoring js

--- assets/script.js
+++ assets/script.js
@@ -16,15 +16,13 @@ const searchBtn = document.getElementById("searchBtn");
 const clearBtn = document.getElementById("clearBtn");
 const searchInput = document.getElementById("keywords");
 
-const searchHistory = document.getElementById("search-history")
+var searchHistory = document.getElementById("past_searches")
 
 var userInputArr = JSON.parse(localStorage.getItem("savedSearches")) || [];
 var storageInput = JSON.parse(localStorage.getItem("savedSearches") || "[]");
 var lastSearchedInput = storageInput.at(-1) || "Melbourne";
 console.log("storageInput", storageInput);
 console.log("lastSearchedInput", lastSearchedInput);
-
-
 
 var userLatitude;
 var userLongitude;
@@ -33,8 +31,6 @@ var userCity;
 // current time and day
 var today = moment();
 currentTimeDiv.textContent = today.format("dddd, Do MMM, YYYY [at] h:mm a");
-
-displayRecentSearches();
 
 
 fetch(geolocationApiUrl)
@@ -53,7 +49,6 @@ fetch(geolocationApiUrl)
 
     initMap(data)
   });
-
 
 
 let map;
@@ -101,15 +96,6 @@ function createMarker(place) {
   });
 }
 
-
-var city = "Melbourne"
-
-
-var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?&classificationName=music&city=" + city + "&apikey=" + apiKeyTM
-function getApi() {
-
-
-
 myBtn.addEventListener("click", function searchCity(event){ 
   event.preventDefault()
   var city=document.getElementById("keywords").value
@@ -119,10 +105,12 @@ myBtn.addEventListener("click", function searchCity(event){
   else{
     localStorage.setItem("Location",city)
     initMap()
-    getApi()}
+    getApi()
+  }
   })
 
   myBtn.addEventListener("click",appendCities)
+ 
  
 //retrieving events data and displaying them  
 function getApi(){
@@ -168,79 +156,17 @@ function getApi(){
       document.getElementById('keywords').value = ""
       var city=node.textContent 
       localStorage.setItem("Location",city) 
+      
       initMap()
       getApi()
       })
     }
     
-    
-
-      for (var i = 0; i < 5; i++) {
-        var displayEl = document.createElement('p')
-        eventsListDiv.append(displayEl)
-        displayEl.innerHTML = "<strong>Name:</strong>" + (data._embedded.events[i].name) + '<br>'
-        displayEl.innerHTML += "<strong>Address:</strong>" + JSON.stringify((data._embedded.events[i]._embedded.venues[0].address.line1)) + "<br>"
-        //displayEl.innerHTML+=JSON.stringify((data._embedded.events[i]._embedded.venues[0].location))+"<br>"
-        displayEl.innerHTML += "<strong>Date:</strong>" + (data._embedded.events[i].dates.start.localDate) + " <br>"
-        displayEl.innerHTML += "<strong>Time:</strong>" + (data._embedded.events[i].dates.start.localTime) + "<br/>"
-
-      }
-    })
-}
-
 getApi()
 
 // clearBtn
-
 clearBtn.addEventListener("click", function () {
-  userInputArr = [];
-  localStorage.removeItem("savedSearches");
+  //userInputArr = [];
+  localStorage.removeItem("Location");
   searchHistory.innerText = "";
-
-});
-
-// searchBtn 
-searchBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  console.log(searchInput.value);
-
-  if (searchInput.value === '' || !isNaN(searchInput.value)) {
-    alert('Please Enter a city or a keyword.');
-    return;
-  } else if (searchInput.value) {
-    saveRecentSearches();
-    getApi(searchInput.value);
-  }
-  searchInput.value = "";
-
-});
-
-// saving resent searches in local storage
-function saveRecentSearches() {
-  var userSearchInput = searchInput.value;
-  userInputArr.push(userSearchInput);
-  localStorage.setItem("savedSearches", JSON.stringify(userInputArr));
-  displayRecentSearches();
-
-}
-
-// displaying resent 5 searches in html (need to set css for btn class)
-function displayRecentSearches() {
-  searchHistory.innerHTML = "";
-  const recentFiveSearch = userInputArr.slice(-5);
-  recentFiveSearch.forEach(function (item) {
-    const historyBtn = document.createElement("button");
-    historyBtn.textContent = item;
-    historyBtn.style.textTransform = "capitalize";
-    historyBtn.setAttribute("class", "btn");
-    searchHistory.appendChild(historyBtn);
-
-    historyBtn.addEventListener("click", function (event) {
-      console.log(event.target.textContent);
-      const clickedCity = event.target.textContent;
-      getApi(clickedCity);
-    })
-
-  })
-}
-
+  });

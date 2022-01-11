03396a9bdd27e232cc963970ea183a109be3b899
Nico Pasqualini <pasqualinin9@gmail.com>
2022-01-11 15:52:05 +1100

    display events and map based on user input

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,7 @@ const currentTimeDiv = document.getElementById("current-date-time");
 const container = document.getElementById("container");
 const mapDiv = document.getElementById("map");
 const eventsListDiv = document.getElementById("events-list");
+var myBtn=document.getElementById("searchBtn")
 var userLatitude;
 var userLongitude;
 var userCity;
@@ -37,41 +38,81 @@ fetch(geolocationApiUrl)
 
 
 let map;
+let service;
+let infowindow;
 
 function initMap(data) {
   console.log("initMap passing:", data);
   console.log("userLatitude:", userLatitude);
   console.log("userLongitude:", userLongitude);
+  infowindow = new google.maps.InfoWindow();
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: userLatitude, lng: userLongitude },
     zoom: 12,
   });
+  const request = {
+    query: localStorage.getItem("Location"),
+    fields: ["name", "geometry"],
+  };
+  
+  service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+      for (let i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+
+      map.setCenter(results[0].geometry.location);
+    }
+  });
 }
 
-var city ="Melbourne"
+function createMarker(place) {
+  if (!place.geometry || !place.geometry.location) return;
+
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", () => {
+    infowindow.setContent(place.name || "");
+    infowindow.open(map);
+  });
+}
 
 
 
-var queryURL="https://app.ticketmaster.com/discovery/v2/events.json?&classificationName=music&city="+city+"&apikey="+apiKeyTM
+
+myBtn.addEventListener("click", function searchCity(event){ 
+  event.preventDefault()
+  var city=document.getElementById("keywords").value
+    localStorage.setItem("Location",city)
+    initMap()
+    getApi()
+  })
+ 
+//retrieving events data and displaying them  
 function getApi(){
-    
+ var city=localStorage.getItem("Location")
+ var queryURL="https://app.ticketmaster.com/discovery/v2/events.json?&classificationName=music&city="+city+"&apikey="+apiKeyTM   
   fetch(queryURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
       console.log(data)
-      for (var i=0;i<5;i++){
+      $("#events-list").html('<div></div>');  
+      for (var i=0;i<10;i++){
         var displayEl= document.createElement('p')
          eventsListDiv.append(displayEl)
-        displayEl.innerHTML="<strong>Name:</strong>"+(data._embedded.events[i].name)+'<br>'
+        displayEl.innerHTML="<strong>Name:</strong>"+(data._embedded.events[i].name)+'<br/>'
         displayEl.innerHTML+="<strong>Address:</strong>"+JSON.stringify((data._embedded.events[i]._embedded.venues[0].address.line1))+"<br>"
         //displayEl.innerHTML+=JSON.stringify((data._embedded.events[i]._embedded.venues[0].location))+"<br>"
-        displayEl.innerHTML+="<strong>Date:</strong>"+(data._embedded.events[i].dates.start.localDate)+" <br>"
+        displayEl.innerHTML+="<strong>Date:</strong>"+(data._embedded.events[i].dates.start.localDate)+" <br/>"
         displayEl.innerHTML+="<strong>Time:</strong>"+(data._embedded.events[i].dates.start.localTime)+"<br/>"
        
       }
   })
 }
  
-getApi()

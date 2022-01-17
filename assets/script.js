4cc7a72caddc321dad62a22989df0232b88bc8bb
Nico Pasqualini <pasqualinin9@gmail.com>
2022-01-17 14:08:06 +1100

    updating input validation and cleaning code

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,24 @@
 // apikeys for google and Ticket Master
 const apiKeyG = "AIzaSyBiZYv6s5ocv7IYkiRQLleoe8MXRsMqtRc";
 const apiKeyTM = "8gCIaWZY4lHT8Aj0NRXlEobIThAGmNGO";
-//var geolocationApiUrl = "http://ipwhois.app/json/";
-
 // variables
 const currentTimeDiv = document.getElementById("current-date-time");
 const container = document.getElementById("container");
 const mapDiv = document.getElementById("map");
 const eventsListDiv = document.getElementById("events-list");
-
 const myBtn=document.getElementById("searchBtn")
 const clearBtn = document.getElementById("clearBtn");
 const searchInput = document.getElementById("keywords");
-//var pastCities = [];
-//const searchBtn = document.getElementById("searchBtn");
 var searchHistory = document.getElementById("past_searches")
- var userInputArr = JSON.parse(localStorage.getItem("savedSearches")) || [];
- var storageInput = JSON.parse(localStorage.getItem("savedSearches") || "[]");
-// var lastSearchedInput = storageInput.at(-1) || "Melbourne";
-// console.log("storageInput", storageInput);
-// console.log("lastSearchedInput", lastSearchedInput);
-
+var userInputArr = JSON.parse(localStorage.getItem("savedSearches")) || [];
+var storageInput = JSON.parse(localStorage.getItem("savedSearches") || "[]");
 var userLatitude;
 var userLongitude;
 var userCity;
-
+var userInputArr=[]
 // current time and day
 var today = moment();
 currentTimeDiv.textContent = today.format("dddd, Do MMM, YYYY [at] h:mm a");
-
-//displayRecentSearches();
-
-// fetch(geolocationApiUrl)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-
-//     userLatitude = data.latitude;
-//     userLongitude = data.longitude;
-//     userCity = data.city;
-
-//     console.log(userLatitude);
-//     console.log(userLongitude);
-
-//     initMap(data)
-//   });
 
 //retrieve user location
 
@@ -55,9 +27,7 @@ let service;
 let infowindow;
 
 function initMap() {
-  //console.log("initMap passing:", data);
-  //console.log("userLatitude:", userLatitude);
-  //console.log("userLongitude:", userLongitude);
+  
   infowindow = new google.maps.InfoWindow();
   
   //setting map on a default value
@@ -87,53 +57,17 @@ function initMap() {
             map,
             title: "Current Location",
           });
-          //infoWindow.setPosition(pos);
-          //infoWindow.setContent("Location found.");
-          //infoWindow.open(map);
+          
           map.setCenter(pos);
         },
         () => {
           //handleLocationError(true, infoWindow, map.getCenter());
         }
       );
-    } //else {
-      // Browser doesn't support Geolocation
-     
-     // handleLocationError(false, infoWindow, map.getCenter());
-  //  }
+    } 
   });
 }
 
-//function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  // alert("Access to location denied")
-   //infoWindow.setPosition(pos);
-  //infoWindow.setContent(
-  //   browserHasGeolocation
-  //     ? "Error: The Geolocation service failed."
-  //     : "Error: Your browser doesn't support geolocation."
-  // );
-  // infoWindow.open(map);
-//}
- 
- 
-//}
-
- // const request = {
-   // query: localStorage.getItem("Location"),
-    //fields: ["name", "geometry"],
-  //};
-  
-  //service = new google.maps.places.PlacesService(map);
-  //service.findPlaceFromQuery(request, (results, status) => {
-  // if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-     //for (let i = 0; i < results.length; i++) {
-       // createMarker(results[i]);
-     // }
-
-      //map.setCenter(results[0].geometry.location);
-    //}
-  //});
-//}
 
 
 function createMarker(place) {
@@ -144,19 +78,7 @@ function createMarker(place) {
     position: place.geometry.location,
   });
 
-  // google.maps.event.addListener(marker, "click", () => {
-  //   infowindow.setContent(place.name || "You are here!");
-  //   infowindow.open(map);
-  // });
 }
-
-
-// var city = "Melbourne"
-
-
-// var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?&classificationName=music&city=" + city + "&apikey=" + apiKeyTM
-// function getApi() {
-
 
 
 myBtn.addEventListener("click", function searchCity(event){ 
@@ -167,8 +89,7 @@ myBtn.addEventListener("click", function searchCity(event){
     return;}
   else{
     localStorage.setItem("Location",city)
-    //pastCities.push(city)
-    //localStorage.setItem("past cities", JSON.stringify(pastCities))
+    
     initMap()
     getApi()
   }
@@ -181,9 +102,7 @@ myBtn.addEventListener("click",saveRecentSearches)
 function getApi(){
   //new map with the location we insert
   function initMap1(data) {
-    //console.log("initMap passing:", data);
-    //console.log("userLatitude:", userLatitude);
-    //console.log("userLongitude:", userLongitude);
+    
     infowindow = new google.maps.InfoWindow();
     map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: userLatitude, lng: userLongitude },
@@ -219,20 +138,23 @@ fetch(queryURL)
     console.log(data)
 
   $("#events-list").html('<div></div>'); 
+//USER DATA VALIDATION
 //checking if there are events on the user city  
   if(data.page.totalElements==0){
+    if (!('_embedded' in data)){
     var displayEl= document.createElement('p')
     eventsListDiv.append(displayEl)
-    displayEl.innerHTML="No Events for this city"
+    displayEl.innerHTML="No Events for this city or city does not exist"
   }
-  else{ 
+    //else {alert("City not found")}
+}
+  
+    else{ 
       for (var i=0;i<10;i++){
         var displayEl= document.createElement('p')
         eventsListDiv.append(displayEl)
         displayEl.innerHTML="<strong>Name:</strong>"+(data._embedded.events[i].name)+'<br/>'
-        //displayEl.innerHTML+="<strong>Address:</strong>"+JSON.stringify((data._embedded.events[i]._embedded.venues[0].address.line1))+"<br>"
-        //displayEl.innerHTML+=JSON.parse((data._embedded.events[i]._embedded.venues[0].location.latitude))+"<br>"
-        //displayEl.innerHTML+=JSON.parse((data._embedded.events[i]._embedded.venues[0].location.longitude))+"<br>"
+        
         displayEl.innerHTML+="<strong>Date:</strong>"+(data._embedded.events[i].dates.start.localDate)+" <br/>"
         displayEl.innerHTML+="<strong>Time:</strong>"+(data._embedded.events[i].dates.start.localTime)+"<br/>"
         
@@ -243,6 +165,7 @@ fetch(queryURL)
       } 
     }
   })
+  
 }    
 
 var markerImage = "./assets/music-note-icon.png";
@@ -272,129 +195,38 @@ function appendCities(event) {
           localStorage.setItem("Location",city) 
          
       
-      
-      
-      //var node = document.createElement("h1");
-      
-      //node.setAttribute("id","cityname")
-      //node.textContent=localStorage.getItem("Location");
-      //document.getElementById("past_searches").appendChild(node);
-      
-      //node.addEventListener("click",function(){
-      //document.getElementById('keywords').value = ""
-      //var city=node.textContent 
-      //localStorage.setItem("Location",city) 
-     
-      
-      //citiesToShow=pastCities.splice(-5)
-// console.log(citiesToShow)
-// for(var i=0;i<citiesToShow.length;i++){
-//     var x=document.createElement("p")
-//     x.textContent=citiesToShow[i]
-//     mydiv.append(x)
-// }
+
       initMap()
       getApi()
       })
 }}
     
     
-
-      // for (var i = 0; i < 5; i++) {
-      //   var displayEl = document.createElement('p')
-      //   eventsListDiv.append(displayEl)
-      //   displayEl.innerHTML = "<strong>Name:</strong>" + (data._embedded.events[i].name) + '<br>'
-      //   displayEl.innerHTML += "<strong>Address:</strong>" + JSON.stringify((data._embedded.events[i]._embedded.venues[0].address.line1)) + "<br>"
-      //   //displayEl.innerHTML+=JSON.stringify((data._embedded.events[i]._embedded.venues[0].location))+"<br>"
-      //   displayEl.innerHTML += "<strong>Date:</strong>" + (data._embedded.events[i].dates.start.localDate) + " <br>"
-      //   displayEl.innerHTML += "<strong>Time:</strong>" + (data._embedded.events[i].dates.start.localTime) + "<br/>"
-
-      // }
-    //}
-
-
-
-
-// var pastCities = [];
-// var mydiv=document.querySelector("#past_searches")
-
-// function myFunction(){
-//   //alert("ciao")  
-
-//   pastCities[0] = "rome";
-//   pastCities[1]="milan"
-// //console.log(names)
-// pastCities.push("vienna")
-// pastCities.push("berlin")
-// pastCities.push("prague")
-// pastCities.push ("madrid")
-// pastCities.push ("barcelona")
-// pastCities.push ("munchen")
-// pastCities.push("london")
-// console.log(pastCities)
-// //display the last 2 values
-
-// localStorage.setItem("past cities", JSON.stringify(pastCities))
-
-// citiesToShow=pastCities.splice(-5)
-// console.log(citiesToShow)
-// for(var i=0;i<citiesToShow.length;i++){
-//     var x=document.createElement("p")
-//     x.textContent=citiesToShow[i]
-//     mydiv.append(x)
-// }
-
-// }
-
-
 clearBtn.addEventListener("click",function(){
-  localStorage.removeItem("savedSearches", JSON.stringify(userInputArr));    
+  localStorage.removeItem("savedSearches");    
+  localStorage.removeItem("Location")
   searchHistory.innerHTML = ""
+  userInputArr=[]
  })
 
 
-// myFunction()
-
-
-
-
-
-
-
-//function () {
-  //userInputArr = [];
-  ///localStorage.removeItem("Location");
-  //searchHistory.innerText = "";
-  //searchHistory.removeChild(historyBtn);
-  //$("past_searches").html('<div></div>'); 
-
-//});
-
-// searchBtn 
-// searchBtn.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   console.log(searchInput.value);
-
-//   if (searchInput.value === '' || !isNaN(searchInput.value)) {
-//     alert('Please Enter a city or a keyword.');
-//     return;
-//   } else if (searchInput.value) {
-//     saveRecentSearches();
-//     getApi(searchInput.value);
-//   }
-//   searchInput.value = "";
-
-// });
 
 // saving resent searches in local storage
-function saveRecentSearches() {
+function saveRecentSearches(data) {
   var userSearchInput = document.getElementById("keywords").value
-  if(!userInputArr.slice(-5).includes(userSearchInput)){
-  userInputArr.push(userSearchInput);
-}
+  
+  if(userSearchInput!==""){
+    if(isNaN(userSearchInput)){
+    
+     if(!userInputArr.slice(-5).includes(userSearchInput)){
+    //
+      //(city === '' || !isNaN(city)
+     userInputArr.push(userSearchInput);
+
   localStorage.setItem("savedSearches", JSON.stringify(userInputArr));
   displayRecentSearches();
-
+    }}
+  }
 }
 
 //displaying resent 5 searches in html (need to set css for btn class)
@@ -418,13 +250,10 @@ function displayRecentSearches() {
     }
     )
     
-    // historyBtn.addEventListener("click", function (event) {
-    //   console.log(event.target.textContent);
-    //   const clickedCity = event.target.textContent;
-    //   getApi(clickedCity);
-    // })
-
+  
   })
 }
 
 getApi()
+
+
